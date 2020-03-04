06bcd88cb6a728648856210d1ddb75fd6aa91219
bontib <tfgcguys@gmail.com>
2020-03-04 12:44:37 +0100

    Validation toegevoegd

--- src/components/subcomponents/login/form.js
+++ src/components/subcomponents/login/form.js
@@ -1,20 +1,48 @@
 import React from "react";
 import { Form as F } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
+import { useForm } from "react-hook-form";
 
 export default function Form() {
   const { t } = useTranslation();
+  const { register, errors, handleSubmit } = useForm();
+
+  function Submit(event) {
+    event.preventdefault();
+  }
 
   return (
-    <F>
+    <F onSubmit={handleSubmit(Submit)}>
       <F.Group controlId="formBasicUsername">
         <F.Label>{t("Login.Username")}</F.Label>
-        <input type="text" className="form-control" id="username" />
+        <input
+          type="text"
+          className="form-control"
+          id="username"
+          name="username"
+          ref={register({
+            required: true
+          })}
+        />
+        {errors.username && errors.username.type === "required" && (
+          <span className="text-danger">{t("Login.UsernameError")}</span>
+        )}
       </F.Group>
 
       <F.Group controlId="formBasicPassword">
         <F.Label>{t("Login.Password")}</F.Label>
-        <input type="password" className="form-control" id="password"></input>
+        <input
+          type="password"
+          className="form-control"
+          id="password"
+          name="password"
+          ref={register({
+            required: true
+          })}
+        ></input>
+        {errors.username && errors.username.type === "required" && (
+          <span className="text-danger">{t("Login.PasswordError")}</span>
+        )}
       </F.Group>
 
       <F.Group controlId="formBasicSubmit">
