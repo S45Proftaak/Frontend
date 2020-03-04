c59a4eb93697074dd0e2ae4a1046b75ddf6508e4
bontib <tfgcguys@gmail.com>
2020-03-04 11:56:02 +0100

    navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/navbar';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const {t} = useTranslation();
+
+
   return (
      <div className="App-header">
+       <Router>
+         <Navbar Navs={[
+           {link: "/", name: t('App.Home')}
+         ]}/>
+
+
+         <Switch>
+           <Route exact key='/'></Route>
+         </Switch>
+       </Router>
       </div>
   );
 }
