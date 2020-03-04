f3d809fbae4f1e3f882f3ac7a0627671d8b8c510
bontib <tfgcguys@gmail.com>
2020-03-04 12:28:25 +0100

    Login gemaakt en displayed

--- src/components/subcomponents/login/form.js
+++ src/components/subcomponents/login/form.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import { Form as form } from 'react-bootstrap';
-import { useTranslation } from 'react-i18next';
+import React from "react";
+import { Form as F } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
 
-export default function Form(){
-    
-    const { t } = useTranslation();
+export default function Form() {
+  const { t } = useTranslation();
 
-    return(
-        <div>
-            <form>
-                <form.Group controlId="formBasicUsername">
-                    <form.Label>{t("Login.Username")}</form.Label>
-                    <input type="text" className="form-control" id="username"/>
-                </form.Group>
+  return (
+    <F>
+      <F.Group controlId="formBasicUsername">
+        <F.Label>{t("Login.Username")}</F.Label>
+        <input type="text" className="form-control" id="username" />
+      </F.Group>
 
-                <form.Group controlId="formBasicPassword">
-                    <form.Label>{t("Login.Password")}</form.Label>
-                    <input type="password" className="form-control" id="password"></input>
-                </form.Group>
+      <F.Group controlId="formBasicPassword">
+        <F.Label>{t("Login.Password")}</F.Label>
+        <input type="password" className="form-control" id="password"></input>
+      </F.Group>
 
-                <form.Group controlId="formBasicSubmit">
-                    <button type="submit" className="btn btn-primary">{t("Login.Login")}</button>
-                </form.Group>
-            </form>
-        </div>
-    );
+      <F.Group controlId="formBasicSubmit">
+        <button type="submit" className="btn btn-primary float-right">
+          {t("Login.Login")}
+        </button>
+      </F.Group>
+    </F>
+  );
 }
