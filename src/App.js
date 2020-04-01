779f52f9e86860afcb6af754c39e646288cb0c86
Martin Peeters <406336@student.fontys.nl>
2020-04-01 11:43:46 +0200

    Fixed routing for homepage

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navbar from './components/navbar';
-import { useTranslation } from 'react-i18next';
-import Login from './components/login';
-import Home from './components/Home';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+import { useTranslation } from "react-i18next";
+import Login from "./components/login";
+import Home from "./components/Home";
 
 function App() {
-  const {t} = useTranslation();
-
+  const { t } = useTranslation();
 
   return (
-     <div className="App-header">
-       <Router>
-         <Navbar Navs={[
-           {link: "/", name: t('App.Home')}
-         ]}/>
-         <div style={{margin: 30}}/>
+    <div className="App-header">
+      <Router>
+        <Navbar Navs={[{ link: "/", name: t("App.Home") }]} />
+        <div style={{ margin: 30 }} />
 
-
-         <Switch>
-           <Route exact path='/'></Route>
-           <Route exact path='/login'><Login/></Route>
-         </Switch>
-       </Router>
-      </div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
