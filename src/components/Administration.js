60afd2c52ba995fc73d751e60907727355baad6a
Luc Somers <lucsomers@hotmail.com>
2020-04-15 15:14:32 +0200

    AddedRoutingToAdministrationPage

---
+++ src/components/Administration.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { Col, Row, Container } from "react-bootstrap";
+import CurrentSelectedUser from "./subcomponents/Administratie/CurrentSelectedUser";
+import DatePicker from "./subcomponents/Administratie/DatePicker";
+import Overview from "./subcomponents/Administratie/Overview";
+
+class Administration extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <Container>
+        <Row>
+            <Col sm="3">
+              <CurrentSelectedUser />
+            </Col>
+            
+            <Col md="8">
+                <Overview/>
+            </Col>
+        </Row>
+        <Row>
+            <Col sm="3">
+              <DatePicker />
+            </Col>
+
+          <Col md="8"></Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Administration;
