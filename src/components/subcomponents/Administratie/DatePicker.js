126e7666b5b5ffaacee25cce9ac05b98c2ecd0b0
Wesley <wesley.verhagen@student.fontys.nl>
2020-05-06 13:39:15 +0200

    Added Base link to DatePicker fetch function
    function is now also async

--- src/components/subcomponents/Administratie/DatePicker.js
+++ src/components/subcomponents/Administratie/DatePicker.js
@@ -6,10 +6,16 @@ import {useSelector} from 'react-redux';
 
 export default function DatePicker(){
     const getUsersBetweenDatesLink = useSelector(state => state.loginReducer.payload.links.secretaryLinks.GET_USERS_BETWEEN_DATES);
+    const baseLink = useSelector(state => state.loginReducer.payload.links.secretaryLinks.BASE);
 
     let fetchUserByDate = async (startDate, endDate) => {
         if(getUsersBetweenDatesLink === undefined){
             console.error("Request link could not be found");
+            return undefined;
+        }
+
+        if(baseLink === undefined){
+            console.error("Base link could not be found");
             return undefined;
         }
 
@@ -18,8 +24,8 @@ export default function DatePicker(){
             return undefined;
         }
 
-        const fullLink = "http://localhost:8020" + undefined + getUsersBetweenDatesLink + "?start=" + startDate + "&end=" + endDate;
-        const result = makeHttpCall(fullLink, requestTypes.GET, undefined);
+        const fullLink = "http://localhost:8020" + baseLink + getUsersBetweenDatesLink + "?start=" + startDate + "&end=" + endDate;
+        const result = await makeHttpCall(fullLink, requestTypes.GET, undefined);
         return result;
     }
 
