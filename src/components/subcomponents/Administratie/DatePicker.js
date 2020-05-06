df2174555844f0e6f01a4c9d26350831198e345d
Wesley <wesley.verhagen@student.fontys.nl>
2020-05-06 13:24:49 +0200

    Implemented DatePicker fetch function
    Missing base URL

--- src/components/subcomponents/Administratie/DatePicker.js
+++ src/components/subcomponents/Administratie/DatePicker.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import {makeHttpCall, requestTypes} from "../../../helpers/httpHelper";
+import {useSelector} from 'react-redux';
+
 
 
 export default function DatePicker(){
+    const getUsersBetweenDatesLink = useSelector(state => state.loginReducer.payload.links.secretaryLinks.GET_USERS_BETWEEN_DATES);
+
+    let fetchUserByDate = async (startDate, endDate) => {
+        if(getUsersBetweenDatesLink === undefined){
+            console.error("Request link could not be found");
+            return undefined;
+        }
+
+        if(startDate === undefined || endDate === undefined){
+            console.error("Missing parameter");
+            return undefined;
+        }
+
+        const fullLink = "http://localhost:8020" + undefined + getUsersBetweenDatesLink + "?start=" + startDate + "&end=" + endDate;
+        const result = makeHttpCall(fullLink, requestTypes.GET, undefined);
+        return result;
+    }
+
     return(
         <div>
 
