b7cfe072e570c4ce3519619aa566e3d1588f910b
Bon Moolenburgh <40125958+bontib@users.noreply.github.com>
2020-05-12 14:13:38 +0200

    fetch fix

--- src/components/subcomponents/Administratie/DatePicker.js
+++ src/components/subcomponents/Administratie/DatePicker.js
@@ -1,46 +1,11 @@
-import React from 'react';
-import {makeHttpCall, requestTypes} from "../../../helpers/httpHelper";
-import {useSelector} from 'react-redux';
-import RangeSelector from './RangeSelector.js';
-import 'react-day-picker/lib/style.css';
+import React from "react";
+import RangeSelector from "./RangeSelector.js";
+import "react-day-picker/lib/style.css";
 
-export default function DatePicker(){
-
-    let getUsersBetweenDatesLink = null;
-    let baseLink = null;
-    let start = undefined;
-    let end = undefined;
-
-    const basemodel = useSelector(state => state.loginReducer.payload.links);
-    if(basemodel !== undefined){
-        getUsersBetweenDatesLink = basemodel.secretaryLinks.GET_USERS_BETWEEN_DATES;
-        baseLink = basemodel.secretaryLinks.BASE;
-    }
-
-    let fetchUserByDate = async (startDate, endDate) => {
-        if(getUsersBetweenDatesLink === undefined){
-            console.error("Request link could not be found");
-            return undefined;
-        }
-
-        if(baseLink === undefined){
-            console.error("Base link could not be found");
-            return undefined;
-        }
-
-        if(startDate === undefined || endDate === undefined){
-            console.error("Missing parameter");
-            return undefined;
-        }
-
-        const fullLink = "http://localhost:8020" + baseLink + getUsersBetweenDatesLink + "?start=" + startDate + "&end=" + endDate;
-        const result = await makeHttpCall(fullLink, requestTypes.GET, undefined);
-        return result;
-    }
-
-    return(
-        <div>
-            <RangeSelector fetch={fetchUserByDate}></RangeSelector>
-        </div>
-    );
-  }
+export default function DatePicker() {
+  return (
+    <div>
+      <RangeSelector></RangeSelector>
+    </div>
+  );
+}
--- src/components/subcomponents/Administratie/RangeSelector.js
+++ src/components/subcomponents/Administratie/RangeSelector.js
@@ -1,55 +1,107 @@
 import React, { Component } from "react";
 import { withTranslation } from "react-i18next";
-import "./CSS/RangeSelectorStyle.css"
-import DayPicker, { DateUtils } from 'react-day-picker';
-import 'react-day-picker/lib/style.css';
+import "./CSS/RangeSelectorStyle.css";
+import DayPicker, { DateUtils } from "react-day-picker";
+import "react-day-picker/lib/style.css";
 import { formatFetchDate } from "../../../helpers/dateHelpers";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { makeHttpCall, requestTypes } from "../../../helpers/httpHelper";
 
 class RangeSelector extends Component {
-    constructor(props) {
-      super(props);
-      this.handleDayClick = this.handleDayClick.bind(this);
-      this.handleResetClick = this.handleResetClick.bind(this);
-      this.state = this.getInitialState();
+  constructor(props) {
+    super(props);
+    this.handleDayClick = this.handleDayClick.bind(this);
+    this.handleResetClick = this.handleResetClick.bind(this);
+    this.state = this.getInitialState();
+  }
+
+  getInitialState() {
+    return {
+      from: undefined,
+      to: undefined,
+    };
+  }
+
+  handleDayClick(day) {
+    const range = DateUtils.addDayToRange(day, this.state);
+    this.setState({
+      payload: this.fetchUserByDate(
+        formatFetchDate(range.from),
+        formatFetchDate(range.to)
+      ),
+    });
+    console.log(this.state.payload);
+  }
+
+  handleResetClick() {
+    this.setState(this.getInitialState());
+  }
+
+  fetchUserByDate = async (startDate, endDate) => {
+    let getUsersBetweenDatesLink = undefined;
+    let baseLink = undefined;
+    if (this.props.links !== undefined) {
+      getUsersBetweenDatesLink = this.props.links.secretaryLinks
+        .GET_USERS_BETWEEN_DATES;
+      baseLink = this.props.links.secretaryLinks.BASE;
+    } else {
+      throw new Error("No links availible");
     }
-  
-    getInitialState() {
-      return {
-        from: undefined,
-        to: undefined,
-      };
+
+    if (getUsersBetweenDatesLink === undefined) {
+      console.error("Request link could not be found");
+      return undefined;
     }
-  
-    handleDayClick(day) {
-      const range = DateUtils.addDayToRange(day, this.state);
-      this.setState(range);
-      console.log(this.state.payload);
+
+    if (baseLink === undefined) {
+      console.error("Base link could not be found");
+      return undefined;
     }
-  
-    handleResetClick() {
-      this.setState(this.getInitialState());
+
+    if (startDate === undefined || endDate === undefined) {
+      console.error("Missing parameter");
+      return undefined;
     }
+
+    const fullLink =
+      "http://localhost:8020" +
+      baseLink +
+      getUsersBetweenDatesLink +
+      "?start=" +
+      startDate +
+      "&end=" +
+      endDate;
+    const result = await makeHttpCall(fullLink, requestTypes.GET, undefined);
+    return result;
+  };
 
   render() {
     const { from, to } = this.state;
     const modifiers = { start: from, end: to };
 
     return (
-        <div className="RangeExample">
-            <DayPicker
-                className="Selectable"
-                numberOfMonths={this.props.numberOfMonths}
-                selectedDays={[from, { from, to }]}
-                modifiers={modifiers}
-                onDayClick={this.handleDayClick}
-            />
-        </div>
+      <div className="RangeExample">
+        <DayPicker
+          className="Selectable"
+          numberOfMonths={this.props.numberOfMonths}
+          selectedDays={[from, { from, to }]}
+          modifiers={modifiers}
+          onDayClick={this.handleDayClick}
+        />
+      </div>
     );
   }
 }
 
-const MyComponent = withTranslation()(RangeSelector);
-
+const MyComponent = compose(
+  withTranslation(),
+  connect((state) => {
+    return {
+      links: state.loginReducer.payload.links,
+    };
+  })
+)(RangeSelector);
 // i18n translations might still be loaded by the xhr backend
 // use react's Suspense
 export default function App() {
