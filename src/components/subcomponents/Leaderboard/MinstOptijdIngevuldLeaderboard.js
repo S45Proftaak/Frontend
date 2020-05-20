9f2883b3ee1d5e867efee1553ea5b8c0d30cc4b8
Bon Moolenburgh <40125958+bontib@users.noreply.github.com>
2020-05-20 11:48:33 +0200

    fetch gemaakt

--- src/components/subcomponents/Leaderboard/MinstOptijdIngevuldLeaderboard.js
+++ src/components/subcomponents/Leaderboard/MinstOptijdIngevuldLeaderboard.js
@@ -6,59 +6,66 @@ import { compose } from "redux";
 import { withTranslation } from "react-i18next";
 
 class MinstOptijdIngevuldLeaderboard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            fetchedData: undefined,
-            fetching: false,
-            fetched: false
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      fetchedData: undefined,
+      fetching: false,
+      fetched: false,
+    };
+  }
 
+  GetScoreboardValues = () => {
+    this.setState({
+      fetching: true,
+    });
+    makeHttpCall(
+      "http://localhost:8020/scoreboard/get-scoreboard-too-late",
+      this.props.token,
+      requestTypes.GET
+    ).then((response) => {
+      this.setState({
+        fetching: false,
+        fetched: true,
+        fetchedData: response,
+      });
+    });
+  };
 
-    GetScoreboardValues = () => {
-        this.setState({
-            fetching: true,
-        });
-        makeHttpCall(
-            "http://localhost:8020/scoreboard/get-scoreboard-too-late",
-            this.props.token,
-            requestTypes.GET,
-        ).then((response) => {
-            this.setState({
-                fetching: false,
-                fetched: true,
-                fetchedData: response,
-            });
-        });
-    }
-
-    render() {
-        return (
-            <Container>
-                <h5 className="text-center">
-                    Minst Op tijd Ingevuld
-                </h5>
-                <div className ="text-center">
-                    {this.state.fetchedData}
+  render() {
+    return (
+      <Container>
+        <h5 className="text-center">Minst Op tijd Ingevuld</h5>
+        <div className="text-center">
+          {this.state.fetched ? (
+            <div>
+              {this.state.fetchedData.map((res) => (
+                <div>
+                  {res.user.name}
+                  {res.user.role.name}
+                  {res.user.totalPoints}
                 </div>
-            </Container>
-                );
-    }
-
-
+              ))}
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </div>
+      </Container>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    console.log(state.loginReducer.payload.token);
-    return {
-        token: state.loginReducer.payload.token,
-    }
+  console.log(state.loginReducer.payload.token);
+  return {
+    token: state.loginReducer.payload.token,
+  };
 }
 
 const MyComponent = compose(
-    withTranslation(),
-    connect(mapStateToProps)
+  withTranslation(),
+  connect(mapStateToProps)
 )(MinstOptijdIngevuldLeaderboard);
 
 export default MyComponent;
